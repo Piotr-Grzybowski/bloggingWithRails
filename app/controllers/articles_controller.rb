0b4e5719a0240700af0a608e57b708a8384437e0
Piotr Grzybowski <pgrzybowski.dev@gmail.com>
2022-03-27 16:33:35 +0200

    Play wit rails

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,10 @@
+# Article controller
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+end
